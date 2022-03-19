b0f7717c36f1d20617ab5684b7ca083fdad4a3ef
Alvar Hansen <alluxxii@gmail.com>
2022-03-19 12:31:06 +0200

    Add SocketRocket

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
          .package(url: "https://github.com/cbaker6/CertificateSigningRequest", from: "1.28.0"),
+         .package(url: "https://github.com/robnadin/SocketRocket", .branchItem("spm-support")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
